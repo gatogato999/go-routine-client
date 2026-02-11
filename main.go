aa9cd3b4039f6e5bb0449ed28184bdbdf090689d
gatogato999 <ggaattoo38@gmail.com>
2026-02-11 14:05:39 +0200

    modified : get data concurrntly

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -18,42 +18,46 @@ type City struct {
 func main() {
 	start := time.Now()
 
-	cities2check := []string{"city1", "city2", "city4"}
+	cities2check := os.Args[1:]
 
+	resultChanal := make(chan string)
 	for _, cityName := range cities2check {
-		fetchCityData(cityName)
+		go fetchCityData(cityName, resultChanal)
+	}
+	for range cities2check {
+		fmt.Println(<-resultChanal)
 	}
 
 	fmt.Printf("all done in %d milliseconds\n", time.Since(start).Milliseconds())
 }
 
-func fetchCityData(cityName string) {
+func fetchCityData(cityName string, ch chan<- string) {
 	sub_start := time.Now()
 
 	url := fmt.Sprintf("http://localhost:3000/cities?name=%s", cityName)
 	res, err := http.Get(url)
 	if err != nil {
-		log.Println(err)
+		ch <- fmt.Sprint(err)
 		return
 	}
 
 	resBody, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
+	res.Body.Close()
 
 	if err != nil {
-		log.Println(err)
+		ch <- fmt.Sprint(err)
 		return
 	}
 
 	var data []City
 	err = json.Unmarshal(resBody, &data)
 	if err != nil {
-		log.Println(err)
+		ch <- fmt.Sprint(err)
 		return
 	}
 
-	fmt.Println("-----------------")
-	fmt.Printf("%s city --> %s degree\t\t", data[0].Name, data[0].Temp)
-	fmt.Printf("%s took %d milliseconds\n", cityName, time.Since(sub_start).Milliseconds())
-	fmt.Print("-----------------\n\n")
+	ch <- fmt.Sprintf("%s city --> %s degree\t\t  took %d milliseconds\n",
+		data[0].Name,
+		data[0].Temp,
+		time.Since(sub_start).Milliseconds())
 }
