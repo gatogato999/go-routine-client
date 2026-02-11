6291fb85cb8a973155b83410b219a99c624d4c04
gatogato999 <ggaattoo38@gmail.com>
2026-02-11 15:52:20 +0200

    modified : handling nonexisting entities

--- main.go
+++ main.go
@@ -18,16 +18,19 @@ type City struct {
 func main() {
 	start := time.Now()
 
+	if len(os.Args) < 2 {
+		os.Exit(2)
+	}
 	cities2check := os.Args[1:]
 
 	resultChanal := make(chan string)
 	for _, cityName := range cities2check {
 		go fetchCityData(cityName, resultChanal)
 	}
+
 	for range cities2check {
 		fmt.Println(<-resultChanal)
 	}
-
 	fmt.Printf("all done in %d milliseconds\n", time.Since(start).Milliseconds())
 }
 
@@ -37,7 +40,7 @@ func fetchCityData(cityName string, ch chan<- string) {
 	url := fmt.Sprintf("http://localhost:3000/cities?name=%s", cityName)
 	res, err := http.Get(url)
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		ch <- fmt.Sprintln(err)
 		return
 	}
 
@@ -45,19 +48,23 @@ func fetchCityData(cityName string, ch chan<- string) {
 	res.Body.Close()
 
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		ch <- fmt.Sprintln(err)
 		return
 	}
 
 	var data []City
 	err = json.Unmarshal(resBody, &data)
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		ch <- fmt.Sprintln(err)
 		return
 	}
 
-	ch <- fmt.Sprintf("%s city --> %s degree\t\t  took %d milliseconds\n",
-		data[0].Name,
-		data[0].Temp,
-		time.Since(sub_start).Milliseconds())
+	if len(data) > 0 {
+		ch <- fmt.Sprintf("%s city --> %s C°\t took %d milliseconds\n\n",
+			data[0].Name,
+			data[0].Temp,
+			time.Since(sub_start).Milliseconds())
+	} else {
+		ch <- fmt.Sprintf("%s doesn't exist\t took %d milliseconds\n\n", cityName, time.Since(sub_start).Milliseconds())
+	}
 }
